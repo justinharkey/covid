de88c668e6356516244bd21462ee43dfe07e4002
Justin Harkey <justin.harkey@financialfinesse.com>
2020-12-07 18:13:56 -0800

    Fixes merge conflicts, removes unused component

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <script lang="ts">
 	import { DataService } from "./services/dataService";
 	import { covidData, selectedCounty, selectedCountyData } from "./stores";
@@ -29,74 +28,13 @@
 			selectedCounty.set(event.state.selectedCountyName);
 		}
 	});
-=======
-{#await getCovidData}
-	<div class="status">Loading data...</div>
-{:then data}
-	<h1 class="graph__title">
-		<CountySelector />
-		<span class="graph__subtitle">New daily cases from March 3rd, 2020</span>
-		<a id="graph__source" href="https://github.com/datadesk/california-coronavirus-data/blob/master/latimes-county-totals.csv">Source: LA Times</a>
-	</h1>
-	<Chart />
-{:catch error}
-	<div class="status">Unable to retrieve data.</div>
-{/await}
-
-<script lang="ts">
-import Chart from './Chart.svelte';
-import CountySelector from './CountySelector.svelte';
-import { COUNTY_LIST } from './constants';
-import { parsedData, selectedCounty } from './stores';
-import type { ICounty } from './models/county';
-
-/**
- * Checks if a pathname is present, if so checks to see if pathname matches county in county list. Updates Svelte store if there is a match.
- */
-const setCountyByUrl = () => {
-	const currentUrl: string = window.location.pathname.substr(1);
-	if (currentUrl) {
-		let filteredCounty: Array<ICounty> = COUNTY_LIST.counties.filter((county) => {
-			return county.countySlug === currentUrl;
-		});
-		selectedCounty.set(filteredCounty[0].countyName);
-	}
-}
-
-/**
- * Parses CSV data into an array.
- * @param {string} covidData - CSV data
- * @returns {array} Array with an array of parsed data.
- */
-const parseCSVData = (covidData: string) => {
-	// regex from https://gist.github.com/Jezternz/c8e9fafc2c114e079829974e3764db75
-	const objPattern = new RegExp(("(\\,|\\r?\\n|\\r|^)(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|([^\\,\\r\\n]*))"),"gi");
-	let arrMatches = null, arrData = [[]];
-	while (arrMatches = objPattern.exec(covidData)){
-		if (arrMatches[1].length && arrMatches[1] !== ",")arrData.push([]);
-		arrData[arrData.length - 1].push(arrMatches[2] ? 
-			arrMatches[2].replace(new RegExp( "\"\"", "g" ), "\"") :
-			arrMatches[3]);
-	}
-	parsedData.set(arrData);
-	return arrData;
-}
->>>>>>> a9e445bf0a7aff6a0402bfbeb86940296449b101
 
 	setCountyByUrl();
 	DataService.getData();
 
-<<<<<<< HEAD
 	$: {
 		document.title = `${$selectedCounty} County - New Daily Coronavirus Cases`;
 		DataService.filterSelectedCountyData($selectedCounty, $covidData);
-=======
-setCountyByUrl();
-
-window.addEventListener('popstate', (event: PopStateEvent) => {
-	if (event.state && event.state.selectedCountyName) {
-		selectedCounty.set(event.state.selectedCountyName);
->>>>>>> a9e445bf0a7aff6a0402bfbeb86940296449b101
 	}
 </script>
 
--- src/CountySelector.svelte
+++ src/CountySelector.svelte
@@ -1,75 +1,47 @@
-<<<<<<< HEAD
 <script lang="ts">
-    import { COUNTY_LIST } from "./constants";
-    import { selectedCounty } from "./stores";
+  import { COUNTY_LIST } from "./constants";
+  import { selectedCounty } from "./stores";
 
-    $: selectedCountyName = $selectedCounty;
+  $: selectedCountyName = $selectedCounty;
 
-    /**
-     * Updates history, document title, and store upon user selection of a new county.
-     * @param {object} event - event object
-     */
-    const updateSelectedCounty = (event: Event) => {
-        const target = event.target as HTMLSelectElement;
-        history.pushState(
-            { selectedCountyName: target.value },
-            "",
-            `/${target.value.replace(/ /g, "-").toLowerCase()}`
-        );
-        document.title = `${target.value} County - New Daily Coronavirus Cases`;
-        selectedCounty.set(target.value);
-    };
-=======
-<select bind:value={selectedCountyName} on:change={updateSelectedCounty}>
-    {#each COUNTY_LIST.counties as county}
-        <option value={county.countyName}>{county.countyName} County</option>
-    {/each}
-</select>
-
-<script lang="ts">
-import { COUNTY_LIST } from './constants';
-import { parsedData, selectedCounty } from './stores';
-
-$: selectedCountyName = $selectedCounty;
-
-/**
- * Updates history, document title, and store upon user selection of a new county.
- * @param {object} event - event object
- */
-const updateSelectedCounty = (event: Event) => {
+  /**
+   * Updates history, document title, and store upon user selection of a new county.
+   * @param {object} event - event object
+   */
+  const updateSelectedCounty = (event: Event) => {
     const target = event.target as HTMLSelectElement;
-    history.pushState({'selectedCountyName': target.value}, '', `/${target.value.replace(/ /g, '-').toLowerCase()}`);
+    history.pushState(
+      { selectedCountyName: target.value },
+      "",
+      `/${target.value.replace(/ /g, "-").toLowerCase()}`
+    );
     document.title = `${target.value} County - New Daily Coronavirus Cases`;
     selectedCounty.set(target.value);
-}
->>>>>>> a9e445bf0a7aff6a0402bfbeb86940296449b101
+  };
 </script>
 
 <style>
-    select {
-        font-size: 18px;
-        font-weight: bold;
-        margin: 0;
-        padding: 3px 0;
-        border: 1px solid #ddd;
-        border-radius: 3px;
-        margin: 4px 0;
-    }
+  select {
+    font-size: 18px;
+    font-weight: bold;
+    margin: 0;
+    padding: 3px 0;
+    border: 1px solid #ddd;
+    border-radius: 3px;
+    margin: 4px 0;
+  }
 </style>
 
 <div class="count count--county">
-    <h1 class="count__title">Covid-19 Data For</h1>
-    <!-- svelte-ignore a11y-no-onchange -->
-    <select bind:value={selectedCountyName} on:change={updateSelectedCounty}>
-        {#each COUNTY_LIST.counties as county}
-            <option value={county.countyName}>
-                {county.countyName}
-                County
-            </option>
-        {/each}
-    </select>
-    <a
-        class="count__subtitle"
-        href="https://github.com/datadesk/california-coronavirus-data/blob/master/latimes-county-totals.csv">Source:
-        LA Times</a>
+  <h1 class="count__title">Covid-19 Data For</h1>
+  <!-- svelte-ignore a11y-no-onchange -->
+  <select bind:value={selectedCountyName} on:change={updateSelectedCounty}>
+    {#each COUNTY_LIST.counties as county}
+      <option value={county.countyName}>{county.countyName} County</option>
+    {/each}
+  </select>
+  <a
+    class="count__subtitle"
+    href="https://github.com/datadesk/california-coronavirus-data/blob/master/latimes-county-totals.csv">Source:
+    LA Times</a>
 </div>
